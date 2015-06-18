29f1c2b98b03e7415d4b1b0763cb341f2411d98e
Ivan Wills <ivan.wills@gmail.com>
2015-06-18 17:56:55 +1000

    Made boilerplate test work

--- lib/App/Git/Workflow/Command/Cat.pm
+++ lib/App/Git/Workflow/Command/Cat.pm
@@ -14,7 +14,7 @@ use English qw/ -no_match_vars /;
 use App::Git::Workflow;
 use App::Git::Workflow::Command qw/get_options/;
 
-our $VERSION  = 0.2;
+our $VERSION  = 0.1;
 our $workflow = App::Git::Workflow->new;
 our ($name)   = $PROGRAM_NAME =~ m{^.*/(.*?)$}mxs;
 our %option;
@@ -42,93 +42,44 @@ __DATA__
 
 =head1 NAME
 
-App::Git::Workflow::Command::Cat - <One-line description of module's purpose>
+App::Git::Workflow::Command::Cat - Show the content of a git file.
 
 =head1 VERSION
 
-This documentation refers to App::Git::Workflow::Command::Cat version 0.0.1
-
+This documentation refers to App::Git::Workflow::Command::Cat version 0.1
 
 =head1 SYNOPSIS
 
-   use App::Git::Workflow::Command::Cat;
+   git-since-release [option]
 
-   # Brief but working code example(s) here showing the most common usage(s)
-   # This section will be as far as many users bother reading, so make it as
-   # educational and exemplary as possible.
+ OPTIONS:
+  -q --quiet    Suppress notifying of files changed
 
+  -v --verbose  Show more detailed option
+     --VERSION  Prints the version information
+     --help     Prints this help information
+     --man      Prints the full documentation for git-since-release
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
-
-May include numerous subsections (i.e., =head2, =head3, etc.).
-
+C<git-since-release> finds out how many commits the repository is since the
+latest release (determined by the latest tag).
 
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
+=head2 C<run ()>
 
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
-
-Name the section accordingly.
-
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
-
-
-=head3 C<new ( $search, )>
-
-Param: C<$search> - type (detail) - description
-
-Return: App::Git::Workflow::Command::Cat -
-
-Description:
-
-=cut
-
+Run the command
 
 =head1 DIAGNOSTICS
 
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
-
 =head1 CONFIGURATION AND ENVIRONMENT
-
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
 
 =head1 DEPENDENCIES
 
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
-
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
